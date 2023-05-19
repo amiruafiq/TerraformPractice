d2a2e6ff313229ca01a7973cae983f9127b17998
Afiq Kurshid <amirulafiqkk@gmail.com>
2023-05-19 09:25:40 +0800

    python automation

--- project 3 - Build Web using k8/main.tf
+++ project 3 - Build Web using k8/main.tf
@@ -1,82 +1,39 @@
-# Configure the AWS provider
 provider "aws" {
-  region = "us-west-2"
-  access_key = "xx"
-  secret_key = "xxx"  
+    region = "ap-southeast-1"
+    access_key = "xxx"
+    secret_key = "xxx"  
 }
 
-# Create a Kubernetes cluster
-module "eks" {
-  source = "terraform-aws-modules/eks/aws"
-  
-  cluster_name = "my-cluster"
-  subnets      = ["subnet-xxxxxxxxxxxxxx", "subnet-yyyyyyyyyyyyyy", "subnet-zzzzzzzzzzzzzz"]
-  vpc_id       = "vpc-xxxxxxxxxxxxxxxxx"
-}
+# 1. Create S3 bucket
+resource "aws_s3_bucket" "ab" {
+  bucket = "bucket-to-test-static-website"
 
-# Deploy a simple website to the Kubernetes cluster
-resource "kubernetes_deployment" "website" {
-  metadata {
-    name = "website"
-    labels = {
-      app = "website"
-    }
-  }
-
-  spec {
-    replicas = 3
-
-    selector {
-      match_labels = {
-        app = "website"
-      }
-    }
-
-    template {
-      metadata {
-        labels = {
-          app = "website"
-        }
-      }
-
-      spec {
-        container {
-          image = "nginx:latest"
-          name  = "website"
-          port {
-            container_port = 80
-          }
-        }
-      }
-    }
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+    Project     = "static web"
   }
 }
 
-# Expose the website using a LoadBalancer service
-resource "kubernetes_service" "website_lb" {
-  metadata {
-    name = "website-lb"
-  }
+# 2. upload index.html file to bucket
 
-  spec {
-    selector = {
-      app = kubernetes_deployment.website.spec.0.template.0.metadata.0.labels.app
-    }
 
-    port {
-      name        = "http"
-      port        = 80
-      target_port = 80
-    }
+# 3. make bucket public
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.ab.id
 
-    type = "LoadBalancer"
-  }
-
-  depends_on = [
-    kubernetes_deployment.website
-  ]
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
-# Note that this code assumes that you have already set up your AWS credentials 
-# and that you have created the necessary subnets and VPC for your EKS cluster. 
-# Also, you may need to modify the region, subnets, and VPC ID based on your specific setup.
+
+# 4. Enable s3 static website hosting
+resource "aws_s3_bucket_website_configuration" "example" {
+  bucket = aws_s3_bucket.ab.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
